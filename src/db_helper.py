43c735b1e6c01d94cc68197d0369268d982e805e
Tatu Laras <tatu.laras@gmail.com>
2024-12-09 02:37:01 +0200

    Yksi poistetuista asioista olikin testien käytössä, palautettu

---
+++ src/db_helper.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from sqlalchemy import text
+
+from config import db, app, SCHEMA_NAME
+
+
+def setup_db():
+    # Luo uusi skeema tiedoston /src/schema.sql pohjalta
+    path = Path(__file__).parent / "../schema.sql"
+    with path.open() as fp:
+        sql = fp.read()
+        db.session.execute(text(sql))
+
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        setup_db()
